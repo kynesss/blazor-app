2eb473ef2a8ace8efcea0e809509178252700f21
Kynes <kamilpirog98@gmail.com>
2023-07-07 14:48:24 +0200

    test2

--- Api/AnnouncementFunction.cs
+++ Api/AnnouncementFunction.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Net;
+using System.Net.Http;
 using System.Web;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
@@ -9,7 +10,7 @@ namespace ApiIsolated;
 
 public static class AnnouncementFunction
 {
-    private static readonly HttpClient HttpClient = new();
+    private static HttpClient _httpClient;
 
     //url
     private const string FunctionUrl = "http://localhost:7071/api/CustomMediaPostsTrigger?url=";
@@ -18,15 +19,21 @@ public static class AnnouncementFunction
     public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
         FunctionContext executionContext)
     {
+        _httpClient = new HttpClient();
+        
         var logger = executionContext.GetLogger("AnnouncementFunction");
         logger.LogInformation("C# HTTP trigger function processed a request.");
 
         var query = HttpUtility.ParseQueryString(req.Url.Query);
         var url = query.Get("url");
         
-        var httpRequest = new HttpRequestMessage(HttpMethod.Post, $"{FunctionUrl}{url}");
-        var httpResponse = await HttpClient.SendAsync(httpRequest);
+        //var httpRequest = new HttpRequestMessage(HttpMethod.Post, $"{FunctionUrl}{url}");
+        //var httpResponse = await _httpClient.SendAsync(httpRequest);
+
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        await response.WriteStringAsync($"{FunctionUrl}{url}");
         
-        return req.CreateResponse(httpResponse.StatusCode);
+        return response;
     }
 }

b93885b646fd927b07034bf306a9e874ca0fccce
Kynes <kamilpirog98@gmail.com>
2023-07-07 15:09:05 +0200

    test4

--- Api/AnnouncementFunction.cs
+++ Api/AnnouncementFunction.cs
@@ -12,28 +12,26 @@ public static class AnnouncementFunction
 {
     private static HttpClient _httpClient;
 
-    //url
-    private const string FunctionUrl = "http://localhost:7071/api/CustomMediaPostsTrigger?url=";
-
     [Function("AnnouncementFunction")]
     public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
         FunctionContext executionContext)
     {
         _httpClient = new HttpClient();
-        
+
         var logger = executionContext.GetLogger("AnnouncementFunction");
         logger.LogInformation("C# HTTP trigger function processed a request.");
 
         var query = HttpUtility.ParseQueryString(req.Url.Query);
         var url = query.Get("url");
-        
-        //var httpRequest = new HttpRequestMessage(HttpMethod.Post, $"{FunctionUrl}{url}");
-        //var httpResponse = await _httpClient.SendAsync(httpRequest);
 
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-        await response.WriteStringAsync($"{FunctionUrl}{url}");
-        
-        return response;
+        if (string.IsNullOrEmpty(url))
+        {
+            // Handle the case when "url" parameter is missing or empty
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        }
+
+        // Continue with your logic using the "url" parameter value
+
+        return req.CreateResponse(HttpStatusCode.OK);
     }
 }

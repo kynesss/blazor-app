47aa8875c3d1001d3af3c51fb8caf61eeb61c8ca
Kynes <kamilpirog98@gmail.com>
2023-07-10 10:38:40 +0200

    test10

--- Api/AnnouncementFunction.cs
+++ Api/AnnouncementFunction.cs
@@ -15,36 +15,20 @@ public static class AnnouncementFunction
     private const string FunctionUrl = "http://localhost:7071/api/CustomMediaPostsTrigger";
 
     [Function("AnnouncementFunction")]
-    public static async Task<HttpResponseData> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")]
-        HttpRequestData req, FunctionContext executionContext)
+    public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
+        FunctionContext executionContext)
     {
-        HttpClient client = new HttpClient();
+        _httpClient = new HttpClient();
 
-        string functionUrl = "http://localhost:7071/api/CustomMediaPostsTrigger";
-        string queryParameter =
-            "https://www.onet.pl/informacje/onetwiadomosci/rzez-wolynska-kleska-dyplomatyczna-i-moralna-andrzeja-dudy/dtztlx3,79cfc278";
-        string urlWithQuery = $"{functionUrl}?url={Uri.EscapeDataString(queryParameter)}";
+        var announcementUrl = await req.ReadAsStringAsync();
 
-        try
-        {
-            HttpResponseMessage response = await client.GetAsync(urlWithQuery);
-            response.EnsureSuccessStatusCode();
-            string responseContent = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(responseContent);
-        }
-        catch (HttpRequestException ex)
-        {
-            Console.WriteLine($"Request failed with exception: {ex.Message}");
-            if (ex.InnerException != null)
-            {
-                Console.WriteLine($"Inner exception: {ex.InnerException.Message}");
-            }
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An error occurred: {ex.Message}");
-        }
+        if (string.IsNullOrEmpty(announcementUrl))
+            return req.CreateResponse(HttpStatusCode.BadRequest);
+        
+        var urlWithQuery = $"{FunctionUrl}?url={Uri.EscapeDataString(announcementUrl)}";
+
+        HttpContent content = new StringContent(urlWithQuery);
+        await _httpClient.PostAsync(FunctionUrl, content);
 
         return req.CreateResponse(HttpStatusCode.OK);
     }

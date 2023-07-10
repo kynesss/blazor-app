0ff2c83f02bb14db73b57b22927ed48da27fdf50
Kynes <kamilpirog98@gmail.com>
2023-07-10 09:17:02 +0200

    test5

--- Api/AnnouncementFunction.cs
+++ Api/AnnouncementFunction.cs
@@ -1,6 +1,5 @@
 ﻿using System.Net;
 using System.Net.Http;
-using System.Web;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -13,16 +12,16 @@ public static class AnnouncementFunction
     private static HttpClient _httpClient;
 
     [Function("AnnouncementFunction")]
-    public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
+    public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req,
         FunctionContext executionContext)
     {
+        //url jest nullem dlatego nie dziala
         _httpClient = new HttpClient();
 
         var logger = executionContext.GetLogger("AnnouncementFunction");
         logger.LogInformation("C# HTTP trigger function processed a request.");
-
-        var query = HttpUtility.ParseQueryString(req.Url.Query);
-        var url = query.Get("url");
+        
+        var url = await req.ReadAsStringAsync();
 
         if (string.IsNullOrEmpty(url))
         {
